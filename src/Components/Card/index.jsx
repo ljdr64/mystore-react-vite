d580575e65d482204a553a6a63b71134b3d724b0
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-05-30 08:42:50 -0300

    feat: opening and closing ProductDetail on product click

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -3,10 +3,13 @@ import { useContext } from 'react';
 import { ShoppingCartContext } from '../Context';
 
 const Card = (data) => {
-  const { count, setCount } = useContext(ShoppingCartContext);
+  const context = useContext(ShoppingCartContext);
 
   return (
-    <div className="bg-white cursor-pointer w-56 h-60">
+    <div
+      className="bg-white cursor-pointer w-56 h-60 rounded-lg"
+      onClick={() => context.openProductDetail()}
+    >
       <figure className="relative mb-2 w-full h-4/5">
         <span className="absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-sm m-2 px-3 py-0.5">
           {data.data?.category}
@@ -18,7 +21,10 @@ const Card = (data) => {
         />
         <div
           className="absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1"
-          onClick={() => setCount(count + 1)}
+          onClick={(e) => {
+            e.stopPropagation();
+            context.setCount(context.count + 1);
+          }}
         >
           <FaPlus />
         </div>
--- src/Components/ProductDetail/index.jsx
+++ src/Components/ProductDetail/index.jsx
@@ -1,12 +1,23 @@
+import { useContext } from 'react';
+import { ShoppingCartContext } from '../Context';
 import './style.css';
 import { FaTimes } from 'react-icons/fa';
 
 const ProductDetail = () => {
+  const context = useContext(ShoppingCartContext);
+
   return (
-    <aside className="product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white">
+    <aside
+      className={`${
+        context.isProductDetailOpen ? 'flex' : 'hidden'
+      } product-detail flex flex-col fixed right-0 border border-black rounded-lg bg-white`}
+    >
       <div className="flex justify-between items-center p-6">
         <h2 className="font-medium text-xl">Detail</h2>
-        <div>
+        <div
+          className="cursor-pointer"
+          onClick={() => context.closeProductDetail()}
+        >
           <FaTimes />
         </div>
       </div>
