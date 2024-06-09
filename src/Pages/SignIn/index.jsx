6c7bf039a6b84b3921afbbcd9068e72e0fdd9aa6
Laureano Rodriguez <laure.jd0101@gmail.com>
2024-06-09 11:21:34 -0300

    feat: adding functionality for user account detection, enabling/disabling buttons

--- src/Pages/SignIn/index.jsx
+++ src/Pages/SignIn/index.jsx
@@ -1,7 +1,24 @@
+import { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import Layout from '../../Components/Layout';
+import { ShoppingCartContext } from '../../Context';
 
 function SignIn() {
+  const context = useContext(ShoppingCartContext);
+
+  // Account
+  const account = localStorage.getItem('account');
+  const parsedAccount = JSON.parse(account);
+
+  // Has an account
+  const noAccountInLocalStorage = parsedAccount
+    ? Object.keys(parsedAccount).length === 0
+    : true;
+  const noAccountInLocalState = parsedAccount
+    ? Object.keys(context.account).length === 0
+    : true;
+  const hasUserAnAccount = !noAccountInLocalStorage || !noAccountInLocalState;
+
   return (
     <Layout>
       <div className="flex items-center justify-center relative w-80 mb-4">
@@ -15,6 +32,7 @@ function SignIn() {
               <label className="block text-sm font-bold mb-2" htmlFor="email">
                 Email:
               </label>
+              <span>{parsedAccount?.email}</span>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="email"
@@ -29,6 +47,7 @@ function SignIn() {
               >
                 Password:
               </label>
+              <span>{parsedAccount?.password}</span>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="password"
@@ -36,26 +55,42 @@ function SignIn() {
                 placeholder="********"
               />
             </div>
-            <div className="flex flex-col items-center text-center mb-2">
-              <Link
-                to="/"
-                className="bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full"
+            <Link to="/">
+              <button
+                className={`
+                  ${
+                    !hasUserAnAccount
+                      ? 'pointer-none bg-gradient-to-r from-gray-200 to-gray-300'
+                      : 'text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800'
+                  } font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mb-2`}
+                disabled={!hasUserAnAccount}
               >
                 Sign In
-              </Link>
+              </button>
+            </Link>
+            <div className="text-center text-sm mb-6">
+              <span
+                className={`
+                  ${hasUserAnAccount ? 'hover:text-blue-700' : ''}`}
+              >
+                Forgot my password
+              </span>
             </div>
           </form>
-          <div className="text-center text-sm mb-6">
-            <a href="#" className="hover:text-blue-700">
-              Forgot my password
-            </a>
-          </div>
-          <div className="flex flex-col items-center text-center">
-            <Link
-              to="/sign-up"
-              className="bg-gradient-to-r from-gray-200 to-gray-300 hover:from-gray-300 hover:to-gray-400 font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mb-2"
-            >
-              Sign Up
+
+          <div className="text-center">
+            <Link to="/sign-up">
+              <button
+                className={`
+                  ${
+                    hasUserAnAccount
+                      ? 'pointer-none bg-gradient-to-r from-gray-200 to-gray-300'
+                      : 'text-white bg-gradient-to-r from-blue-500 to-blue-700 hover:from-blue-600 hover:to-blue-800'
+                  } font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full mb-2`}
+                disabled={hasUserAnAccount}
+              >
+                Sign Up
+              </button>
             </Link>
             <span className="text-sm">Do not you have an account?</span>
           </div>
